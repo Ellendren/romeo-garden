4bb36dfea9ccd61f1e380e7854847f818632aa87
Ellendren <lukesh@sonic.net>
2024-08-12 15:57:30 -0700

    added python script to make config.toml for the build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,7 @@ source $cargo_source
 echo -e "${BLUE}running rustup"
 rustup update
 
+# make the config.toml fole for the build
+python3 scripts/conifure-toml.py $MYSQLUSER $MYSQLPASS
+
 cargo build --release
