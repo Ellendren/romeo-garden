8d9d02c931d7a4c02a8b8dcf120ca5f5567bf57b
Ellendren <lukesh@sonic.net>
2024-08-11 23:09:59 -0700

    added build script

---
+++ scripts/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BLUE='\033[0;34m'
+
+echo -e "${BLUE}Starting build script"
+
+#download rustc if not found
+if ! [ -x "$(command -v rustc)" ]
+then
+    RUSRTUP="tmp-rustup.sh"
+    echo -e "${BLUE}Intsalling rustc"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $RUSRTUP
+    chmod a+x $RUSRTUP
+    ./$RUSRTUP -y
+    rm $RUSRTUP
+    source ~/.cargo/env
+else
+    echo -e "${BLUE}rustc is installed"
+fi
+
+echo -e "${BLUE}running rustup"
+rustup update
+
+cargo build
