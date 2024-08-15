ba968fe7fc451a2d4522fb36c645ba8a6ac883cb
Ellendren <lukesh@sonic.net>
2024-08-14 17:09:46 -0700

    added cli for garden CRUD

--- src/cli/garden.rs
+++ src/cli/garden.rs
@@ -1,14 +1,89 @@
 use mysql::Pool;
+use colored::Colorize;
 
-use crate::database::garden_controller::view_gardens;
+use crate::database::garden_controller;
 
 pub fn garden_cmd(args: &Vec<String>, pool: Pool) {
+    //default to view command if no commands passed
     if args.len() == 0 {
-        match view_gardens(pool) {
-            Ok(res) => println!("{:?}", res),
-            Err(e) => eprintln!("{:?}", e)
-        };
+        view_gardens(pool);
 
         return;
     }
+
+    //check command if exists
+    let cmd = args[0].clone();
+    match cmd.as_str() {
+        "view" => view_gardens(pool),
+        "add" => add_garden(pool, args.iter().nth(1).unwrap_or(&"".to_string())),
+        "rm" => rm_garden(pool, args.iter().nth(1).unwrap_or(&"".to_string())),
+        "new-name" => {
+            let mut args_iter = args.iter();
+            args_iter.next();
+            new_name(pool, args_iter.next().unwrap_or(&"".to_string()), args_iter.next().unwrap_or(&"".to_string()))
+        },
+        "help" => help(),
+        _ => eprintln!("{} command '{cmd}' not found for garden. Run 'garden help; to see availavle commands", "Error:".red())
+    }
+}
+
+fn view_gardens(pool: Pool) {
+    match garden_controller::view_gardens(pool) {
+        Ok(res) => println!("{:?}", res),
+        Err(e) => eprintln!("{:?}", e)
+    };
+}
+
+fn add_garden(pool: Pool, name: &String) {
+    if name.len() == 0 {
+        eprintln!("{}: no name for garden add. Expected garden add <name>", "Error".red());
+
+        return;
+    }
+
+    match garden_controller::add_garden(pool, name) {
+        Ok(_) => {},
+        Err(e) => eprintln!("{}: {:?}", "Error".red(), e)
+    }
+}
+
+fn rm_garden(pool: Pool, name: &String) {
+    if name.len() == 0 {
+        eprintln!("{}: no name for garden rm. Expected: `garden rm <name>`", "Error".red());
+
+        return;
+    }
+
+    match garden_controller::remove_garden(pool, name) {
+        Ok(_) => {},
+        Err(e) => eprintln!("{}: {:?}", "Error".red(), e)
+    }
+}
+
+fn new_name(pool: Pool, old_name: &String, new_name: &String) {
+    if old_name.len() == 0 || new_name.len() == 0 {
+        eprintln!("{}: Two arguments expected for garden new-name. Expected: `garden new-name <old_name> <new_name>`", "Error".red());
+
+        return;
+    }
+
+    match garden_controller::new_name_garden(pool, old_name, new_name) {
+        Ok(_) => {},
+        Err(e) => eprintln!("{}: {:?}", "Error".red(), e)
+    }
+}
+
+fn help() {
+    let help = 
+    r#"garden help: 
+    useage:
+        garden [command] [options]
+
+    commands:
+        view
+        add <garden name>
+        rm <garden name>
+        new-name <old name> <new name>"#;
+
+    println!("{help}");
 }
