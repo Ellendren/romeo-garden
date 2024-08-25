b7eeaba72d12f3d914b3f01a80f766fe251568d7
Ellendren <lukesh@sonic.net>
2024-08-25 15:58:09 -0700

    added add and drop container methods to ConatinerController

--- src/database/container_controller.rs
+++ src/database/container_controller.rs
@@ -34,15 +34,15 @@ impl ContainerController {
         };
         let width = match width {
             Some(val) => val,
-            None => 0.0
+            None => 12.0
         };
         let length = match length {
             Some(val) => val,
-            None => 0.0
+            None => 12.0
         };
         let height = match height {
             Some(val) => val,
-            None => 0.0
+            None => 12.0
         };
         let volume = match volume {
             Some(val) => val,
@@ -87,6 +87,22 @@ impl ContainerController {
     pub fn view_container(self, pool: Pool) -> Result<Vec<ContainerController>, Error> {
         view_container(pool, &self.container_name, &self.garden_name)
     }
+
+    pub fn add_container_bed(self, pool: Pool) -> Result<(), Error> {
+        add_container_bed(
+            pool, 
+            &self.container_name,
+            &self.garden_name,
+            self.width, 
+            self.length, 
+            self.height, 
+            self.volume
+        )
+    }
+
+    pub fn drop_container(self, pool: Pool) -> Result<(), Error> {
+        drop_container(pool, &self.container_name, &self.garden_name)
+    }
 }
 
 pub fn view_container(pool: Pool, container_name: &str, garden_name: &str) -> Result<Vec<ContainerController>, Error> {
@@ -110,15 +126,41 @@ pub fn view_container(pool: Pool, container_name: &str, garden_name: &str) -> Re
     })
 }
 
-// pub fn add_container_bed(pool: Pool, cname: &str, gname: &str) -> Result<(), Error> {
-//     let query = format!("CALL add_bed('')");
-//     query_drop(pool, &query)
-// }
+pub fn add_container_bed(
+    pool: Pool, 
+    cname: &str, 
+    gname: &str,
+    width: f64,
+    length: f64,
+    height: f64,
+    volume: f64
+) -> Result<(), Error> {
+    let query = format!("CALL add_bed(
+            \"{cname}\",
+            \"{gname}\",
+            \"{width}\",
+            \"{length}\",
+            \"{height}\",
+            \"{volume}\")"
+        );
+    query_drop(pool, &query)
+}
+
+pub fn drop_container(pool: Pool, cname: &str, gname: &str) -> Result<(), Error> {
+    let query = format!("CALL drop_container(\"{cname}\", \"{gname}\")");
+    query_drop(pool, &query)
+}
 
 #[cfg(test)]
 mod tests {
-    use super::view_container;
+    use super::{
+        view_container,
+        ContainerController
+    };
     use super::super::connection;
+
+    const TEST_CNAME: &str = "container_name";
+    const TEST_GNAME: &str = "garden_name";
 
     #[test]
     fn view_container_test() {
@@ -127,6 +169,53 @@ mod tests {
 
         let res = view_container(pool.clone(), "", "");
         println!("{:?}", res);
-        assert!(res.is_ok())
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn add_container_bed_test() {
+        let conn = connection::Connection::new(None);
+        let pool = conn.get_pool();
+
+        //create garden for tests
+        crate::database::garden_controller::add_garden(pool.clone(), TEST_GNAME).unwrap();
+
+        let test_container: ContainerController = ContainerController::new(
+            TEST_CNAME.to_string(),
+            TEST_GNAME.to_string(),
+            None, 
+            None, 
+            None, 
+            None, 
+            None
+        );
+
+        let res = test_container.add_container_bed(pool.clone());
+        println!("{:?}", res);
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn drop_container() {
+        let conn = connection::Connection::new(None);
+        let pool = conn.get_pool();
+
+        let test_container: ContainerController = ContainerController::new(
+            TEST_CNAME.to_string(),
+            TEST_GNAME.to_string(),
+            None, 
+            None, 
+            None, 
+            None, 
+            None
+        );
+
+        let res = test_container.drop_container(pool.clone());
+        println!("{:?}", res);
+
+        //remove test garden
+        crate::database::garden_controller::remove_garden(pool.clone(), TEST_GNAME).unwrap();
+
+        assert!(res.is_ok());
     }
 }
