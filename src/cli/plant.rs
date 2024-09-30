19ca2c242bf03a30105e4767cea45dc237d54771
Ellendren <lukesh@sonic.net>
2024-09-30 15:59:55 -0700

    added add plant cli command

--- src/cli/plant.rs
+++ src/cli/plant.rs
@@ -1,11 +1,14 @@
+use mysql::serde_json::Error;
 use mysql::Pool;
 use colored::Colorize;
 
-use crate::database::plant_controller;
+use crate::database::plant_controller::{self, Plant};
+use crate::cli::{prompt_input, prompt_input_f64};
 
 pub fn plant_cmd(args: &Vec<String>, pool: Pool) {
     let cmd = if args.len() > 0 {args[0].clone()} else {format!("no command entered")};
     match cmd.as_str() {
+        "add" => add_plant(pool, &args[1..].to_vec()),
         "view" => {
             if args.len() == 2 {
                 let plant_id = args[1].parse().unwrap_or(0);
@@ -17,6 +20,67 @@ pub fn plant_cmd(args: &Vec<String>, pool: Pool) {
         },
         "help" => help(),
         _ => eprintln!("{} command '{cmd}' not found for plant. Run 'container help' to see availavle commands", "Error:".red())
+    }
+}
+
+fn add_plant(pool: Pool, args: &Vec<String>) {
+    println!("{}", "Enter container info".green());
+
+    let mut container_name = None;
+    let mut garden_name = None;
+    let mut location = None;
+    let mut species = None;
+    let mut variety = None;
+
+    for arg in args.iter() {
+        let option: Vec<&str> = arg.split('=').collect();
+
+        if option.len() == 2{
+            let (key, val) = (option[0], option[1]);
+
+            match key {
+                "container" => container_name = Some(val.to_string().chars().filter(|c| *c != '\"').collect()),
+                "garden" => garden_name = Some(val.to_string().chars().filter(|c| *c != '\"').collect()),
+                "location" => location = Some(val.to_string().parse::<f64>().unwrap()),
+                "species" => species = Some(val.to_string().chars().filter(|c| *c != '\"').collect()),
+                "variety" => variety = Some(val.to_string().chars().filter(|c| *c != '\"').collect()),
+                _ => {
+                    eprintln!("{} no option {}", "Err:".red(), key);
+                    return;
+                }
+            }
+        }
+        else {
+            eprintln!("{} no value for option {}", "Err:".red(), option[0]);
+                    return;
+        }
+    }
+    let garden_name = garden_name.unwrap_or_else(|| {prompt_input("Garden name: ")});
+    let container_name = container_name.unwrap_or_else(|| {prompt_input("Container name: ")});
+    let location = match location {
+        Some(val) => Some(val),
+        None => prompt_input_f64("length: ")
+    };
+    let species = species.unwrap_or_else(|| {prompt_input("Species: ")});
+    let variety = variety.unwrap_or_else(|| {prompt_input("Variety: ")});
+
+    println!("{}", "Adding container".green());
+
+    let new_plant = plant_controller::Plant::new(
+        None, 
+        None, 
+        Some(container_name), 
+        Some(garden_name), 
+        location, 
+        Some(species), 
+        Some(variety), 
+        None, 
+        None
+    );
+    
+    match new_plant.add(pool) {
+        Ok(_) => {}
+        Err(e) => eprint!("{}: {}", "Error".red(), e)
     }
 }
 
